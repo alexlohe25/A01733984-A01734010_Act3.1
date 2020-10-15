bee9fd82337ab567c7f1976ba9d7eab2551ca1d0
alexlohe25 <62221946+alexlohe25@users.noreply.github.com>
2020-10-14 22:42:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,13 +5,32 @@ struct Node{
     struct Node* izq;
     struct Node* der;
 };
-
+void preOrden(Node* r){
+    if (r != NULL){
+        cout << r -> info << " ";
+        preOrden(r -> izq);
+        preOrden(r -> der);
+    }
+}
+void inOrden(Node* r){
+    if (r != NULL){
+        inOrden(r -> izq);
+        cout << r-> info << " ";
+        inOrden(r -> der);
+    }
+}
+void postOrden(Node *r){
+    if (r != NULL){
+        postOrden(r -> izq);
+        postOrden(r -> der);
+        cout << r-> info << " ";
+    }
+}
 Node* generarNodo(int valor){
   Node* tmp = new Node;
   tmp -> info = valor;
   tmp -> der = NULL;
   tmp -> izq = NULL;
-
   return tmp;
 }
 void agregaNodo (Node* &raiz, int valor){
@@ -24,6 +43,22 @@ void agregaNodo (Node* &raiz, int valor){
             agregaNodo(raiz -> izq, valor);
         if(valor > raiz -> info)
             agregaNodo(raiz -> der, valor);
+    }
+}
+void traversal(Node* raiz, int opcion){
+    switch (opcion){
+        case 1:
+            preOrden(raiz);
+            cout << endl;
+        break;
+        case 2:
+            inOrden(raiz);
+            cout << endl;
+        break;
+        case 3:
+            postOrden(raiz);
+            cout << endl;
+        break;
     }
 }
 int whatLevelAmI(Node* raiz, int valor, int nivel){
@@ -39,12 +74,19 @@ int whatLevelAmI(Node* raiz, int valor, int nivel){
 }
 int main(){
     Node* raiz = NULL;
-    agregaNodo(raiz, 5);
-    agregaNodo(raiz, 6);
-    agregaNodo(raiz, 3);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        int valor;
+        cin >> valor;
+        agregaNodo(raiz, valor);
+    }
+    traversal(raiz, 1);
+    traversal(raiz, 2);
+    traversal(raiz, 3);
+    cout << whatLevelAmI(raiz, 30, 0) << endl;
+    cout << whatLevelAmI(raiz, 28, 0) << endl;
+    cout << whatLevelAmI(raiz, 11, 0) << endl;
     cout << whatLevelAmI(raiz, 5, 0) << endl;
-    cout << whatLevelAmI(raiz, 6, 0) << endl;
-    cout << whatLevelAmI(raiz, 3, 0) << endl;
-    cout << whatLevelAmI(raiz, 4, 0) << endl;
     return 0;
 }
