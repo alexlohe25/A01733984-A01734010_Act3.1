f4780781ca815dfb906eda38f5880dc318c7ed52
alexlohe25 <62221946+alexlohe25@users.noreply.github.com>
2020-10-16 23:02:59 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ struct Node{
     int info;
     struct Node* izq;
     struct Node* der;
+    struct Node* padre;
 };
 void preOrden(Node* r){
     if (r != NULL){
@@ -52,23 +53,114 @@ int height(Node* r){
     else
         return ladoDer + 1;
 }
-Node* generarNodo(int valor){
+void ancestors(Node* raiz, Node* nodo, int valor){
+    if (nodo != NULL){
+        if (nodo -> info == valor){
+            while (raiz -> info != valor){
+                cout << raiz -> info << " ";
+                if (valor < raiz -> info)
+                    raiz = raiz -> izq;
+                if (valor > raiz -> info)
+                    raiz = raiz -> der;
+            }
+        }
+        if (valor < nodo -> info)
+            ancestors(raiz, nodo -> izq, valor);
+        if (valor > nodo -> info)
+            ancestors(raiz, nodo -> der, valor);  
+    }
+}
+Node* generarNodo(int valor, Node* padre){
   Node* tmp = new Node;
   tmp -> info = valor;
   tmp -> der = NULL;
   tmp -> izq = NULL;
+  tmp -> padre = padre;
   return tmp;
 }
-void agregaNodo (Node* &raiz, int valor){
+
+void agregaNodo (Node* &raiz, int valor, Node* padre){
     if (raiz == NULL){
-        Node* nodo = generarNodo(valor);
+        Node* nodo = generarNodo(valor, padre);
         raiz = nodo;
     }
     else{
         if(valor < raiz -> info)
-            agregaNodo(raiz -> izq, valor);
+            agregaNodo(raiz -> izq, valor, raiz);
         if(valor > raiz -> info)
-            agregaNodo(raiz -> der, valor);
+            agregaNodo(raiz -> der, valor, raiz);
+    }
+}
+
+void deleteMemoria(Node* node){
+  node -> izq = NULL;
+  node -> der = NULL;
+  delete node;
+}
+
+Node* cambio(Node* raiz){
+  if(raiz == NULL){
+    return NULL;
+  }
+  if(raiz -> izq){
+    return cambio(raiz -> izq);
+  }
+  else{
+    return raiz;
+  }
+}
+
+void reemplaza(Node* raiz, Node* nuevaR){
+  if(raiz -> padre){
+    if(raiz -> info == raiz -> padre -> izq -> info){
+      raiz -> padre -> izq = nuevaR;
+    }
+    if(raiz -> info == raiz -> padre -> der -> info){
+      raiz -> padre -> der = nuevaR;
+    }
+  }
+  if(nuevaR){
+    nuevaR -> padre = raiz -> padre;
+  }
+}
+
+void eliminaNodo(Node* eRaiz){
+  if(eRaiz -> izq && eRaiz -> der){
+    Node* tmp = cambio(eRaiz -> der);
+    eRaiz -> info = tmp -> info;
+    eliminaNodo(tmp);
+  }
+  else if(eRaiz -> izq){
+    reemplaza(eRaiz, eRaiz -> izq);
+    deleteMemoria(eRaiz);
+  }
+  else if(eRaiz -> der){
+    reemplaza(eRaiz, eRaiz -> der);
+    deleteMemoria(eRaiz);
+  }
+  else if(eRaiz -> izq == NULL && eRaiz -> der == NULL){
+    if(eRaiz -> padre -> izq == eRaiz){
+      eRaiz -> padre -> izq = NULL;
+    }
+    else{
+      eRaiz -> padre -> der = NULL;
+    }
+    deleteMemoria(eRaiz);
+  }
+}
+
+void elimina(Node* raiz, int valor){
+    if(raiz == NULL){
+      return;
+    }
+    else if(valor < raiz -> info){
+      elimina(raiz -> izq, valor);
+    }
+    else if(valor > raiz -> info){
+      elimina(raiz -> der, valor);
+    }
+    else{
+      eliminaNodo(raiz);
     }
 }
 void traversal(Node* raiz, int opcion){
@@ -104,21 +196,34 @@ int whatLevelAmI(Node* raiz, int valor, int nivel){
 }
 int main(){
     Node* raiz = NULL;
-    int n;
+    int n, m , q, r;
     cin >> n;
     for (int i = 0; i < n; i++){
-        int valor;
-        cin >> valor;
-        agregaNodo(raiz, valor);
+        int valorAdd;
+        cin >> valorAdd;
+        agregaNodo(raiz, valorAdd, NULL);
     }
-    traversal(raiz, 1);
-    traversal(raiz, 2);
-    traversal(raiz, 3);
-    traversal(raiz, 4);
+    cin >> m;
+    for (int i = 0; i < m; i++){
+        int valorDelete;
+        cin >> valorDelete;
+        elimina(raiz, valorDelete);
+    }
+    for (int i = 1; i <= 4; i++)
+        traversal(raiz,i);
     cout << height(raiz) << endl;
-    cout << whatLevelAmI(raiz, 30, 0) << endl;
-    cout << whatLevelAmI(raiz, 28, 0) << endl;
-    cout << whatLevelAmI(raiz, 11, 0) << endl;
-    cout << whatLevelAmI(raiz, 5, 0) << endl;
+    cin >> q;
+    for (int i = 0; i < q; i++){
+        int valorAncestro;
+        cin >> valorAncestro;
+        ancestors(raiz, raiz, valorAncestro);
+        cout << endl;
+    }
+    cin >> r;
+    for (int i = 0; i < r; i++){
+        int valorNivel;
+        cin >> valorNivel;
+        cout << whatLevelAmI(raiz, valorNivel, 0) << endl;
+    }
     return 0;
 }
